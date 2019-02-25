36c784ba9e178b9c399406927321b09a839fde59
MDay373 <mjday1@email.neit.edu>
2019-02-25 18:56:05 -0500

    Add files via upload

---
+++ Lab6A/server.js
@@ -0,0 +1,34 @@
+require('./models/db');
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+var app = express();
+
+
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/', helpers: require("./public/helpers.js").helpers}));
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, '/views/'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use('/employee', employeeController);
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {    
+    res.render("employee/addOrEdit",{
+        viewTitle: "Enter New Employee",
+        departments: ['Programmer', 'Sales', 'Human Resources']  
+    });    
+});
+
+app.listen(3000, () =>{
+    console.log("Server is running on Port:3000");
+})
+
